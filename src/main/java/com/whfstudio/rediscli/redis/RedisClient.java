ff4ccecdeb26ec28dcec3a0e072ed554066220d6
haifeiWu <whfstudio@163.com>
2019-04-12 15:31:24 +0800

    add termd

--- src/main/java/com/whfstudio/rediscli/redis/RedisClient.java
+++ src/main/java/com/whfstudio/rediscli/redis/RedisClient.java
@@ -25,6 +25,10 @@ import io.netty.handler.codec.redis.RedisBulkStringAggregator;
 import io.netty.handler.codec.redis.RedisDecoder;
 import io.netty.handler.codec.redis.RedisEncoder;
 import io.netty.util.concurrent.GenericFutureListener;
+import io.termd.core.readline.Function;
+import io.termd.core.readline.Keymap;
+import io.termd.core.readline.Readline;
+import io.termd.core.tty.TtyConnection;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -91,7 +95,24 @@ public class RedisClient {
             group.shutdownGracefully();
         }
     }
-//    public static void main(String[] args) throws Exception {
-//
-//    }
+
+    public static void handle(TtyConnection conn) {
+        readline(
+                new Readline(Keymap.getDefault()).addFunctions(Function.loadDefaults()),
+                conn);
+    }
+
+    public static void readline(Readline readline, TtyConnection conn) {
+        readline.readline(conn, "% ", line -> {
+            if (line == null) {
+                conn.write("Logout").close();
+            } else {
+                conn.write("User entered " + line + "\n");
+
+                // Read line again
+                readline(readline, conn);
+            }
+        });
+    }
+
 }
