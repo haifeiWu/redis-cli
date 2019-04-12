03ce7367d991fb04965a3d694e839e3633f6dc73
haifeiWu <whfstudio@163.com>
2019-04-12 23:02:08 +0800

    add cmd line reader

--- src/main/java/com/whfstudio/rediscli/RedisCliApplication.java
+++ src/main/java/com/whfstudio/rediscli/RedisCliApplication.java
@@ -1,9 +1,16 @@
 package com.whfstudio.rediscli;
 
+import com.taobao.middleware.cli.CLI;
+import com.taobao.middleware.cli.CommandLine;
+import com.taobao.middleware.cli.UsageMessageFormatter;
+import com.taobao.middleware.cli.annotations.CLIConfigurator;
 import com.whfstudio.rediscli.redis.RedisClient;
+import com.whfstudio.rediscli.util.UsageRender;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class RedisCliApplication implements CommandLineRunner {
@@ -14,9 +21,27 @@ public class RedisCliApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+
+		CLI cli = CLIConfigurator.define(RedisCliApplication.class);
+		CommandLine commandLine = cli.parse(Arrays.asList(args));
+
+		try {
+			CLIConfigurator.inject(commandLine, RedisCliApplication.class);
+		} catch (Throwable e) {
+			e.printStackTrace();
+			System.out.println(usage(cli));
+			System.exit(1);
+		}
 		String HOST = System.getProperty("host", "127.0.0.1");
 		Integer PORT = Integer.parseInt(System.getProperty("port", "6379"));
 		RedisClient.redisCli(HOST,PORT);
 	}
+
+	private static String usage(CLI cli) {
+		StringBuilder usageStringBuilder = new StringBuilder();
+		UsageMessageFormatter usageMessageFormatter = new UsageMessageFormatter();
+		usageMessageFormatter.setOptionComparator(null);
+		cli.usage(usageStringBuilder, usageMessageFormatter);
+		return UsageRender.render(usageStringBuilder.toString());
+	}
 }
-// java -jar -Dhost=172.16.15.68 -Dport=9221 redis-cli-0.0.1-SNAPSHOT.jar
